25c5b66e2d9e29c4b94b3bdf363523efeebda117
Sumit Ghotane <shubhamghotane9@gmail.com>
2021-07-17 21:41:06 +0530

    Python

---
+++ Chapter 3/P-S.py
@@ -0,0 +1,37 @@
+# Q-1 Write a python program get the userinput and print good moring
+
+Greeting = "Good Afternoon "
+
+# Name = input("Enter the name\n")
+
+# print(Greeting+Name)
+
+# Q-2 Write a python program to fill a letter templete given below with name and date
+
+# date = input("Enter the Date\n")
+
+# letter = f'''
+# Dear {Name}\nYou are selected\n{date}
+#     '''
+# print(letter)
+
+# Q-3 Write a python program to detect the double space in program
+
+Work = "My name  is sumit what is your name "
+
+# print(len(Work))
+# Q-4 Replace the double space from single space
+changes = Work.replace("  ", " ")
+# print(len(changes))
+'''
+As you can see that their are changes in the lenght of the number in work and changes
+
+This show that the replace is successful
+'''
+
+# Q-5 Write a python program to format the string given using escape sequence
+
+String_This = "Dear Harry, This python course is nice.Thanks"
+
+string = "Dear Harry\nThis python course is nice\nThanks!!!"
+print(string)
