b93c837cf23f1a817ec0d00a8f85da590dcf01bb
Sumit Ghotane <shubhamghotane9@gmail.com>
2021-07-18 22:54:17 +0530

    Python

--- Chapter 4/Index.py
+++ Chapter 4/Index.py
@@ -1,25 +1,58 @@
+# List and tuple These are the container in python to stores the data in the string , int , float or boolen
+
 # List
 
-# Initializing the list in [] brackets
-List_1 = [1,2,3,4,5,6,7,8,9,10]
+# Initialize list 
+My_List_1 = [1,5,3,4,2]
 
-# Printing the list 
-# print(List_1)
+# print(My_List_1)
 
-# Get list item by index number and changing index value in list
-List_1[0] = 72
+# This code genrally pring the items inside the list container
 
-# print(List_1[0])
+# List indexing
 
-# We can create list by different types
-c = ["Harry","Ron",72]
+'''
+A list can be index by how the string get index
 
-# print(c)
+1. [0:] - Here the items get print from 0th index to the len of the list
 
-# List Slicing
+2. [0::2] - Here the items get print from the 0th index to the len of the list keeping  
+a 2 index of gap
 
-friends = ["Harry","Ron","Hermoine","Nevile","Luna","Ginny","Fread","Geogre","Smeus","Dean","Wood"]
+! both Methods shown below
+'''
 
-# print(friends[1:7:2])
+# 1. 
 
-print(friends[9:])
+# print(My_List_1[0:])
+
+# another 
+
+# print(My_List_1[:2])
+
+# 2.
+
+# print(My_List_1[0::3]) 
+
+
+# Function in the List
+
+# My_List_1.sort() #Sort the list or arrange the list items form assending to desending order
+
+# My_List_1.reverse() #Reverse function reverse all the items in the string
+
+appending_object = 72
+
+# My_List_1.append(appending_object) # Append or add an object inside the list
+
+My_List_1.insert(3, appending_object)
+
+# Deleting function pop and remove
+
+'''
+.pop remove the element from the index of the list
+
+.remove remove the element value given in the parameter
+'''
+
+print(My_List_1)
---
+++ Chapter 4/P-S.py
@@ -0,0 +1,28 @@
+# Write a python program get the 7 fruits name as input and append it into a fruit list
+
+# Fruit_1 = input("Enter here the fruit name\n")
+# Fruit_2 = input("Enter here the fruit name\n")
+# Fruit_3 = input("Enter here the fruit name\n")
+# Fruit_4 = input("Enter here the fruit name\n")
+# Fruit_5 = input("Enter here the fruit name\n")
+# Fruit_6 = input("Enter here the fruit name\n")
+# Fruit_7 = input("Enter here the fruit name\n")
+
+# Fruit_list = [Fruit_1,Fruit_2,Fruit_3,Fruit_4,Fruit_5,Fruit_6,Fruit_7]
+
+# print(Fruit_list)
+
+# Write a python program which take marks of 6 student and sort them
+
+Student_1 = int(input("Enter here the Student name\n"))
+Student_2 = int(input("Enter here the Student name\n"))
+Student_3 = int(input("Enter here the Student name\n"))
+Student_4 = int(input("Enter here the Student name\n"))
+Student_5 = int(input("Enter here the Student name\n"))
+Student_6 = int(input("Enter here the Student name\n"))
+
+Mark_List = [Student_1,Student_2,Student_3,Student_4,Student_5,Student_6]
+
+sorted_list = Mark_List.sort()
+
+print(Mark_List.sort(reverse=True))
