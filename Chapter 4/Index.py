588cc0c6b43deed0d4e150f3baf430b489585d03
Sumit Ghotane <shubhamghotane9@gmail.com>
2021-07-18 06:12:06 +0530

    Python

---
+++ Chapter 4/Index.py
@@ -0,0 +1,25 @@
+# List
+
+# Initializing the list in [] brackets
+List_1 = [1,2,3,4,5,6,7,8,9,10]
+
+# Printing the list 
+# print(List_1)
+
+# Get list item by index number and changing index value in list
+List_1[0] = 72
+
+# print(List_1[0])
+
+# We can create list by different types
+c = ["Harry","Ron",72]
+
+# print(c)
+
+# List Slicing
+
+friends = ["Harry","Ron","Hermoine","Nevile","Luna","Ginny","Fread","Geogre","Smeus","Dean","Wood"]
+
+# print(friends[1:7:2])
+
+print(friends[9:])
