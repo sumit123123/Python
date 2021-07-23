0f089ee685ca0d43f58aceb9b43226a651ceba1f
Sumit Ghotane <shubhamghotane9@gmail.com>
2021-07-23 19:37:48 +0530

    Python

---
+++ Chapter 7/Index.py
@@ -0,0 +1,53 @@
+"""
+Loops
+
+What is loop?
+When you want to repeat a set of item we can repeat it by loops
+
+Types of loops
+1. For loop
+2. While loop
+
+Syntax
+
+"""
+
+# Syntax of while Loop
+"""
+i = 0
+while i < 10:
+    print("Yes")
+    i = i + 1
+
+print("Done")
+"""
+
+# Quiz-1 Write a python program to print 1 to 50 using while loop
+"""
+i = 0
+
+while i<50+1:
+    print(str(i))
+    i = i + 1
+
+"""
+
+# Another example
+
+"""
+i = 0
+
+while i<5:
+    print("Harry")
+    i = i + 1
+"""
+
+# Quiz-2 Write a python program to type a content of the list using while loops
+
+blank_list = ["Sumit","Harry","Rohan","Sandesh","Ojas"]
+
+i = 0
+
+while i<len(blank_list):
+    print(blank_list[i])
+    i = i + 1
